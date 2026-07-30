45e322c71135b6a94994ed659ddafc8c11f30b8d
dn-creador <registrosbot0@gmail.com>
2026-07-30 10:49:45 -0700

    Create usuarios.js

---
+++ data/usuarios.js
@@ -0,0 +1,35 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/usuarios.js
+=========================================*/
+
+const USUARIOS = [
+
+/*
+{
+    id: "USR-0001",
+
+    correo: "usuario@email.com",
+
+    password: "123456",
+
+    nombre: "Alejandro",
+
+    rol: "Campista",
+
+    personajes: [
+
+        "VXH-0001",
+        "VXH-0002",
+        "VXH-0015"
+
+    ],
+
+    fechaRegistro: "",
+
+    ultimoAcceso: ""
+
+}
+*/
+
+];
