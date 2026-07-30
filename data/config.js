f7bfc78d4488e4ad5ba391d794ab3d0b8e3e927d
dn-creador <registrosbot0@gmail.com>
2026-07-30 10:18:18 -0700

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,47 @@
+/*=========================================
+ CONFIGURACIÓN VEX'HALIA
+=========================================*/
+
+const CONFIG = {
+
+version:"1.0",
+
+nombre:"Campamento Vex'Halia",
+
+maxNivel:100,
+
+monedas:{
+
+denarios:"🪙",
+
+aureos:"💰",
+
+sestercios:"🏺"
+
+},
+
+estadoPersonajes:[
+
+"Pendiente",
+
+"Aprobado",
+
+"Rechazado"
+
+],
+
+roles:[
+
+"Campista",
+
+"Instructor",
+
+"Moderador",
+
+"Administrador",
+
+"Fundador"
+
+]
+
+};
