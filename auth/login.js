48ed2d347520a5285ce4dafa6f6f865ebb0ed1c0
dn-creador <registrosbot0@gmail.com>
2026-07-30 01:26:37 -0700

    Create login.js

---
+++ auth/login.js
@@ -0,0 +1,11 @@
+document.getElementById("entrar").onclick=()=>{
+
+alert("Próximamente iniciaremos sesión.");
+
+}
+
+document.getElementById("registrarse").onclick=()=>{
+
+window.location="../index.html";
+
+}
