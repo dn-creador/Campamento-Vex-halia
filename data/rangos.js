26ec6cac13f8e242db39f996174b1b2f54b2b047
dn-creador <registrosbot0@gmail.com>
2026-07-30 01:11:57 -0700

    Create rangos.js

---
+++ data/rangos.js
@@ -0,0 +1,56 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/rangos.js
+=========================================*/
+
+const RANGOS=[
+
+{
+
+nombre:"Probati",
+
+nivel:1,
+
+xp:0,
+
+color:"#9e9e9e"
+
+},
+
+{
+
+nombre:"Legionario",
+
+nivel:2,
+
+xp:250,
+
+color:"#4caf50"
+
+},
+
+{
+
+nombre:"Centurión",
+
+nivel:3,
+
+xp:1000,
+
+color:"#2196f3"
+
+},
+
+{
+
+nombre:"Campeón Divino",
+
+nivel:4,
+
+xp:5000,
+
+color:"#ffd700"
+
+}
+
+];
