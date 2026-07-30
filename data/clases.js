a82f545684035a38efe7f1bd39fbcfa80ff995b7
dn-creador <registrosbot0@gmail.com>
2026-07-30 00:57:36 -0700

    Create clases.js

---
+++ data/clases.js
@@ -0,0 +1,32 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/clases.js
+=========================================*/
+
+const CLASES=[
+
+{
+nombre:"Probati",
+nivel:1,
+descripcion:"Nuevo integrante del Campamento."
+},
+
+{
+nombre:"Legionario",
+nivel:2,
+descripcion:"Soldado entrenado."
+},
+
+{
+nombre:"Centurión",
+nivel:3,
+descripcion:"Líder de una cohorte."
+},
+
+{
+nombre:"Campeón Divino",
+nivel:4,
+descripcion:"Máximo rango alcanzable."
+}
+
+];
