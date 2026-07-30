487400bc1f8740f3e9b897b78af8f8a72ace905b
dn-creador <registrosbot0@gmail.com>
2026-07-30 00:54:11 -0700

    Create grupos.js

---
+++ data/grupos.js
@@ -0,0 +1,50 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/grupos.js
+=========================================*/
+
+const GRUPOS = [
+
+{
+nombre:"👤 Lobby",
+descripcion:"Registro de nuevos jugadores.",
+link:""
+},
+
+{
+nombre:"⚔️ Grupo Principal",
+descripcion:"Rol principal del Campamento.",
+link:""
+},
+
+{
+nombre:"🏟️ Coliseo",
+descripcion:"Combates y torneos.",
+link:""
+},
+
+{
+nombre:"🌍 Mundo Exterior",
+descripcion:"Rol fuera del Campamento.",
+link:""
+},
+
+{
+nombre:"📬 Buzón",
+descripcion:"Contacto con la administración.",
+link:""
+},
+
+{
+nombre:"🎉 Eventos",
+descripcion:"Eventos oficiales.",
+link:""
+},
+
+{
+nombre:"💬 Spam",
+descripcion:"Conversación libre.",
+link:""
+}
+
+];
