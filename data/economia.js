5fe84b5a5ec203574bfee7ef623281dc2d086230
dn-creador <registrosbot0@gmail.com>
2026-07-30 00:58:15 -0700

    Create economia.js

---
+++ data/economia.js
@@ -0,0 +1,41 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/economia.js
+=========================================*/
+
+const ECONOMIA={
+
+monedas:[
+
+{
+nombre:"Denario",
+icono:"🪙"
+},
+
+{
+nombre:"Áureo",
+icono:"💰"
+},
+
+{
+nombre:"Sestercio",
+icono:"🏺"
+}
+
+],
+
+formasDeGanar:[
+
+"Misiones",
+
+"Eventos",
+
+"Trabajos",
+
+"Torneos",
+
+"Recompensas"
+
+]
+
+};
