2fc601720e8ae96c29cb5c459e98de2a7356db69
dn-creador <registrosbot0@gmail.com>
2026-07-30 01:12:50 -0700

    Create admin.js

---
+++ data/admin.js
@@ -0,0 +1,22 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/admin.js
+=========================================*/
+
+const ADMIN={
+
+estadisticas:{
+
+personajes:0,
+
+pendientes:0,
+
+aprobados:0,
+
+denarios:0
+
+},
+
+noticias:[]
+
+};
