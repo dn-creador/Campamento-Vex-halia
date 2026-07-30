667d0dbb605d15cb5ba299b259b48d4033c10ad5
dn-creador <registrosbot0@gmail.com>
2026-07-30 01:11:13 -0700

    Create personajes.js

---
+++ data/personajes.js
@@ -0,0 +1,44 @@
+/*=========================================
+ CAMPAMENTO VEX'HALIA
+ data/personajes.js
+=========================================*/
+
+const PERSONAJES=[];
+
+/*
+
+Ejemplo de cómo se guardarán:
+
+{
+
+id:"VXH-0001",
+
+usuario:"Alejandro",
+
+nombre:"Marcus",
+
+estado:"Pendiente",
+
+fecha:"30/07/2026",
+
+casa:"Júpiter",
+
+rango:"Probati",
+
+nivel:1,
+
+denarios:0,
+
+aureos:0,
+
+sestercios:0,
+
+inventario:[],
+
+misiones:[],
+
+historial:[]
+
+}
+
+*/
